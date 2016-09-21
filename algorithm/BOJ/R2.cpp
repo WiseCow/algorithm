d897897bcacd35ea54bd66aa63d14aeadf242636
WiseCow <khw2704@gmail.com>
2016-09-21 16:00:53 +0900

    modi

--- algorithm/BOJ/R2.cpp
+++
@@ -1,7 +0,0 @@
-#include<cstdio>
-int main()
-{
-	int r1, s;
-	scanf("%d%d", &r1, &s);
-	printf("%d", 2 * s - r1);
-}
--- algorithm/BOJ/나의 행렬곱셈.cpp
+++
@@ -1,2 +0,0 @@
-#include<cstdio>
-int main(){int a;scanf("%d", &a);printf("3\n1 1 1 %d", a);}
